b67a255de5294fd314e214bfa5b26a20fc45f76c
Luan Teixeira <luan.todos@gmail.com>
2022-05-07 16:13:24 -0300

    refatorado, seguindo principios SOLID

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "29603024d157dc",
+    pass: "cc32da7315f24e"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Luan Teixeira <luan.todos@gmail.com>',
+      subject: subject,
+      html: body,
+    });
+  }
+}
